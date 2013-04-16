d5b3fb2a83fe0df92760cc806eee38019d3f0236
spasovski <davor@spasovski.net>
2013-04-16 12:33:57 -0700

    category featured app listings (bug 860410)

--- hearth/media/js/routes.js
+++ hearth/media/js/routes.js
@@ -9,6 +9,7 @@ var routes = [
     {pattern: '^/app/([^/<>"\']+)$', view_name: 'app'},
     {pattern: '^/search$', view_name: 'search'},
     {pattern: '^/category/([^/<>"\']+)$', view_name: 'category'},
+    {pattern: '^/category/([^/<>"\']+)/featured$', view_name: 'category_featured'},
     {pattern: '^/settings$', view_name: 'settings'},
     {pattern: '^/purchases$', view_name: 'purchases'},
     {pattern: '^/abuse$', view_name: 'abuse'},
---
+++ hearth/media/js/views/category_featured.js
@@ -0,0 +1,20 @@
+define(['capabilities', 'utils', 'z'],
+       function(capabilities, utils, z) {
+
+    var _pd = utils._pd;
+
+    return function(builder, args, __, params) {
+        var category = args[0];
+        params = params || {};
+        _.extend(params, {page: 0});
+
+        builder.z('type', 'search');
+        builder.z('search', params.name || category);
+        builder.z('title', params.name || category);
+
+        builder.start('category/featured.html', {category: category})
+               .done(function() {setTrays();});
+
+    };
+
+});
