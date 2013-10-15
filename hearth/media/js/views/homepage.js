7c13a53dac941f437afb736b16a8320af31dee4a
Davor Spasovski <davor@spasovski.net>
2013-10-15 08:31:17 -0700

    add operator shelves to cat dropdown (bug 923226)

--- hearth/media/js/views/homepage.js
+++ hearth/media/js/views/homepage.js
@@ -1,9 +1,10 @@
 define('views/homepage',
-    ['l10n', 'newsletter', 'underscore', 'urls'],
-    function(l10n, newsletter, _, urls) {
+    ['cat-dropdown', 'format', 'l10n', 'newsletter', 'underscore', 'urls'],
+    function(cdd, format, l10n, newsletter, _, urls) {
     'use strict';
 
     var gettext = l10n.gettext;
+    var operatorInjected = false;
 
     return function(builder, args, params) {
         params = params || {};
@@ -24,6 +25,28 @@ define('views/homepage',
         builder.start('category/main.html', {
             endpoint: urls.api.url('category', [''], params),
             sort: params.sort
-        }).done(newsletter.init);
+        }).done(function() {
+            newsletter.init();
+
+            var catElm = '<li><a class="cat-{0} cat-icon-a" data-cat-slug="{0}" href="{1}">{2}</a></li>';
+
+            cdd.catrequest.done(function() {
+                var $opElm = $('#operator');
+                if (!$opElm.length || operatorInjected) return;
+
+                var opVisible = !!$opElm.filter(':visible').length;
+                var slug = $opElm.data('slug');
+                var name = $opElm.find('header h3').text();
+                var link = $opElm.find('header a').attr('href');
+                var item = format.format(catElm, slug, link, name);
+
+                // We have an op shelf so hide any collection hero. (bug 923236)
+                if (opVisible) $('.collection.main').hide();
+
+                // Inject op shelf to the category dropdown.
+                $('menu.cat-menu').append(item);
+                operatorInjected = true;
+            });
+        });
     };
 });
