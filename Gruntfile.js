dc185d3b8e94d5f0ba62bc8801f0878ee0b752f6
Davor Spasovski <davor@spasovski.net>
2014-04-18 14:05:28 -0400

    fetchdb implementation for fireplace

---
+++ Gruntfile.js
@@ -0,0 +1,39 @@
+var crypto = require('crypto');
+
+var db = require('./lib/db');
+var utils = require('./lib/utils');
+
+module.exports = function (grunt) {
+  grunt.initConfig({
+    fetchdb: {
+      options: {
+        data_dest: '/db/latest.json'
+      }
+    }
+  });
+
+  // Always show stack traces when Grunt prints out an uncaught exception.
+  grunt.option('stack', true);
+
+  grunt.registerTask('fetchdb', 'Fetches JSON from API, downloads ' +
+                                'icons/screenshots, and transforms data to ' +
+                                'static JSON file to disk', function () {
+    var done = this.async();
+    var options = this.options();
+    db.fetchLatest(options.data_dest).then(function () {
+      grunt.log.writeln(
+        'File ' + utils.color('cyan', options.data_dest) + ' created.');
+      done();
+    }, function (err) {
+      grunt.log.writeln(utils.color('red',
+        'File ' + options.file_dest + ' failed to be created: ' + err));
+      done();
+    }).catch(function (err) {
+      grunt.log.writeln(utils.color('red', 'lib/db failed: ' + err));
+      done();
+    });
+
+  });
+
+  grunt.registerTask('default', ['fetchdb']);
+};
---
+++ settings.js
@@ -0,0 +1,5 @@
+var settings = {
+  debug: true
+}
+
+module.exports = settings;
