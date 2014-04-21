9ee3eb75e94565f032802ac09363db9a44d237b7
Davor Spasovski <davor@spasovski.net>
2014-04-21 13:14:36 -0400

    tarako focused image fetching WIP

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,12 @@
-var crypto = require('crypto');
-
 var db = require('./lib/db');
 var utils = require('./lib/utils');
+var settings = require('./settings');
 
-module.exports = function (grunt) {
+module.exports = function(grunt) {
   grunt.initConfig({
     fetchdb: {
       options: {
-        data_dest: '/db/latest.json'
+        data_dest: settings.db_dir + '/latest.json'
       }
     }
   });
@@ -17,18 +16,18 @@ module.exports = function (grunt) {
 
   grunt.registerTask('fetchdb', 'Fetches JSON from API, downloads ' +
                                 'icons/screenshots, and transforms data to ' +
-                                'static JSON file to disk', function () {
+                                'static JSON file to disk', function() {
     var done = this.async();
     var options = this.options();
-    db.fetchLatest(options.data_dest).then(function () {
+    db.fetchLatest(options.data_dest).then(function() {
       grunt.log.writeln(
         'File ' + utils.color('cyan', options.data_dest) + ' created.');
       done();
-    }, function (err) {
+    }, function(err) {
       grunt.log.writeln(utils.color('red',
         'File ' + options.file_dest + ' failed to be created: ' + err));
       done();
-    }).catch(function (err) {
+    }).catch(function(err) {
       grunt.log.writeln(utils.color('red', 'lib/db failed: ' + err));
       done();
     });
--- settings.js
+++ settings.js
@@ -1,5 +1,17 @@
+var dbTransformer = require('./lib/db-transformer');
+
 var settings = {
-  debug: true
-}
+  debug: true,
+  db_url: 'https://marketplace-dev.allizom.org/api/v1/fireplace/collection/curated/?region=restofworld',
+  frontend_dir: 'hearth',
+  use_data_uris: true
+};
+
+settings.db_dir = settings.frontend_dir + '/db';
+settings.downloads_dir = settings.frontend_dir + '/downloads';
+
+settings.db_transformer = function(data) {
+  return dbTransformer(settings, data);
+};
 
 module.exports = settings;
