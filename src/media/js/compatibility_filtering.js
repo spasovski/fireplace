46e95531f84611a2f89294590817bb00b9972d21
Kevin Ngo <ngoke@mozilla.com>
2015-01-22 10:51:09 -0800

    add compat filtering tests for app lists, refactor

--- src/media/js/compatibility_filtering.js
+++ src/media/js/compatibility_filtering.js
@@ -15,8 +15,9 @@ define('compatibility_filtering',
     // API endpoints where feature profile is enabled, if all conditions met.
     // See api_args() below.
     var ENDPOINTS_WITH_FEATURE_PROFILE = [
-        'category_landing','feed', 'feed-app', 'feed-brand', 'feed-collection',
-        'feed-items', 'feed-shelf', 'new_popular_search', 'search'
+        'category_landing', 'feed', 'feed-app', 'feed-brand', 'feed-collection',
+        'feed-items', 'feed-shelf', 'recommended', 'new_popular_search',
+        'search'
     ];
 
     var actual_dev = '';
@@ -128,8 +129,7 @@ define('compatibility_filtering',
                 args.dev = '';
                 args.device = '';
             }
-        }
-        else {
+        } else {
             // If device_filtering_preferences[endpoint_name] does not exist or
             // is an 'empty' value, then we use the default filtering behaviour
             // with whatever dev and device are.
